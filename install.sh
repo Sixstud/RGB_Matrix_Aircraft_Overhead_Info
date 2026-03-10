6c869b4b4883c8bfc9b680a22b79fe630cef85e3
Sixstud <116684239+Sixstud@users.noreply.github.com>
2026-03-10 20:11:00 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,19 +4,22 @@ echo "Updating system"
 sudo apt update
 
 echo "Installing dependencies"
-sudo apt install -y python3-pil python3-requests git
+sudo apt install -y git python3-pil python3-requests curl
 
 echo "Installing RGB matrix driver"
 
 curl -sSL https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/main/rgb-matrix.sh > /tmp/rgb-matrix.sh
 sudo bash /tmp/rgb-matrix.sh
 
-echo "Disabling audio for stable matrix timing"
+echo "Disabling audio for matrix timing"
 
 if ! grep -q "dtparam=audio=off" /boot/config.txt; then
 echo "dtparam=audio=off" | sudo tee -a /boot/config.txt
 fi
 
-echo "Install complete. Please reboot."
+echo "Allowing realtime scheduling"
 
-echo "sudo reboot"
+sudo setcap 'cap_sys_nice=eip' /usr/bin/python3.11
+
+echo "Install complete"
+echo "Reboot required"
